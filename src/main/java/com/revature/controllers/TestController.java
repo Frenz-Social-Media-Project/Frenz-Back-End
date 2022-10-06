c4f5711de9822bb8bb8d158c7c2bbd9f8887a01d
abderahimbahia19@gmail.com <abderrahimbahia19@gmail.com>
2022-10-06 12:58:44 -0400

    added a new controller

--- src/main/java/com/revature/controllers/TestController.java
+++ src/main/java/com/revature/controllers/TestController.java
@@ -1,15 +1,18 @@
 package com.revature.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController()
-@RequestMapping("/")
 public class TestController {
 
-    @GetMapping
-    public String sayHi(){
-        return "Hello from java";
+    @GetMapping("/")
+    public ResponseEntity getServiceName(){
+        ResponseEntity responseEntity = new ResponseEntity(HttpStatus.OK);
+        return responseEntity;
     }
 }
