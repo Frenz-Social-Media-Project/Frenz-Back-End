9d009be4b33fee88a56cadb0f291f31d0c93ada9
Gavin Werner <gavin.werner.2@gmail.com>
2022-10-06 17:22:59 -0400

    pushing up search feature start

--- src/main/java/com/revature/repositories/UserRepository.java
+++ src/main/java/com/revature/repositories/UserRepository.java
@@ -3,9 +3,17 @@ package com.revature.repositories;
 import com.revature.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     Optional<User> findByEmailAndPassword(String email, String password);
+
+
+
+
+
+    // Gavin potential method
+    List<User> findByFirstNameContaining(String keyword);
 }
