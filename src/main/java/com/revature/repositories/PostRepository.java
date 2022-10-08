d1e366521bb59ca6318599a8b96936127d3dfbed
abderahimbahia19@gmail.com <abderrahimbahia19@gmail.com>
2022-10-07 22:19:32 -0400

    testing connection to elastic beanstalk

--- src/main/java/com/revature/repositories/PostRepository.java
+++ src/main/java/com/revature/repositories/PostRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.revature.models.Post;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface PostRepository extends JpaRepository<Post, Integer>{
 
+    Optional<List<Post>> findAllByAuthor_Id(int id);
 }
